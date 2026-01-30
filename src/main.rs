55248815acefa3cfcefe4442ea150e37ad741d68
aldhinn <aldespinas@gmail.com>
2026-01-30 03:23:47 -0600

    implemented CLI workflow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use subnetinfo::ipv4::IPv4Address;
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    let mut empty_args = true;
+
+    for arg in argv.iter() {
+        let ipv4_parse_res = arg.parse::<IPv4Address>();
+        if let Ok(addr) = ipv4_parse_res {
+            empty_args = false;
+            println!("{}", addr.info())
+        }
+    }
+
+    if empty_args {
+        println!("Usage: subnetinfo [IP Addresses]")
+    }
 }
